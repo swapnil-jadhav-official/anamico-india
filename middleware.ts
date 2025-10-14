69c5d721ebb787414e49fc777bc4229f53a7725d
swapnil-jadhav-official <onebox.swapnil@gmail.com>
2025-10-14 10:11:22 +0530

    middleware fixes

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,17 @@
-import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
-export default withAuth(
-  async function middleware(req) {
-    if (req.nextUrl.pathname.startsWith("/admin")) {
-      if (!req.nextauth.token) {
-        return NextResponse.redirect(new URL("/login", req.url));
-      }
-      if (req.nextauth.token.role !== "admin") {
-        return NextResponse.redirect(new URL("/", req.url));
-      }
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (req.nextUrl.pathname.startsWith("/admin")) {
+    if (!token || token.role !== "admin") {
+      return NextResponse.redirect(new URL("/login", req.url));
     }
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
   }
-);
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/admin/:path*"],
