ff070ffc0e13909148847254741603a4327f0e95
swapnil-jadhav-official <onebox.swapnil@gmail.com>
2025-10-14 09:42:57 +0530

    frequently login issue fixed

--- lib/db.ts
+++ lib/db.ts
@@ -2,11 +2,14 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from '../drizzle/schema';
 
-const connection = await mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   database: process.env.DATABASE_DATABASE,
   password: process.env.DATABASE_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-export const db = drizzle(connection, { schema, mode: 'default' });
+export const db = drizzle(pool, { schema, mode: 'default' });
